6a3acee3b5a0b6d8535e2cdd1a3fa6584efe8840
Kim Christensen <kimworking@gmail.com>
2013-09-19 21:07:04 +0200

    Implement ManualFlushWrapper

---
+++ NLog.ManualFlush/NLog.ManualFlush/ManualFlushWrapper.cs
@@ -0,0 +1,27 @@
+﻿using NLog.Common;
+using NLog.Targets.Wrappers;
+using System.Collections.Generic;
+
+namespace NLog.ManualFlush
+{
+    public class ManualFlushWrapper : WrapperTargetBase
+    {
+        private readonly IList<AsyncLogEventInfo> logs = new List<AsyncLogEventInfo>();
+
+        protected override void Write(AsyncLogEventInfo logEvent)
+        {
+            logs.Add(logEvent);
+        }
+
+        protected override void FlushAsync(AsyncContinuation asyncContinuation)
+        {
+            foreach (var log in logs)
+            {
+                WrappedTarget.WriteAsyncLogEvent(log);
+            }
+
+            logs.Clear();
+            base.FlushAsync(asyncContinuation);
+        }
+    }
+}
