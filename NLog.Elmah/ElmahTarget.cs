223144628b27e294837f1893b4502b1d4f435938
Todd Meinershagen <todd@meinershagen.net>
2013-10-02 14:48:11 -0500

    Added target for Elmah and packaging for NuGet.

---
+++ NLog.Elmah/ElmahTarget.cs
@@ -0,0 +1,44 @@
+﻿// Copyright 2013 Kim Christensen, Todd Meinershagen, et. al.
+//  
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
+using Elmah;
+using NLog.Targets;
+
+namespace NLog.Elmah
+{
+    [Target("Elmah")]
+    public sealed class ElmahTarget : TargetWithLayout
+    {
+        private readonly ErrorLog _errorLog;
+
+        public ElmahTarget()
+            : this (ErrorLog.GetDefault(null))
+        {}
+
+        public ElmahTarget(ErrorLog errorLog)
+        {
+            _errorLog = errorLog;
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var logMessage = Layout.Render(logEvent);
+
+            var error = logEvent.Exception == null ? new Error() : new Error(logEvent.Exception);
+            error.Message = logMessage;
+            error.Type = logEvent.Level.ToString();
+            error.Time = logEvent.TimeStamp;
+
+            _errorLog.Log(error);
+        }
+    }
+}
---
+++ buildAndPush.bat
@@ -0,0 +1,13 @@
+cd _packages
+nuget.exe update -self
+ECHO Y | DEL *.nupkg
+
+#NOTE - Using an account for nlog-contrib@meinershagen.net to publish to NuGet.org.  This can be changed in the future.
+nuget.exe setApiKey 11967121-c94f-4d54-9461-c2e441758bc0
+
+#NOTE - If you are only updating one of the packages you can comment out the "nuget.exe pack" statement of the one that you are not deploying.
+nuget.exe pack "..\NLog.Elmah\NLog.Elmah.csproj" -Build -Properties Configuration=Release
+nuget.exe pack "..\NLog.ManualFlush\NLog.ManualFlush.csproj" -Build -Properties Configuration=Release
+
+nuget.exe push *.nupkg
+cd ..
