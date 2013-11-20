a8cbe7b7dd68d512fce37595910d84c678b66c28
Todd Meinershagen <todd@meinershagen.net>
2013-11-19 22:17:03 -0600

    Adding Bruno's name to the authors and adding to the description of the NLog.Contrib project.

--- NLog.Contrib/Properties/AssemblyInfo.cs
+++ NLog.Contrib/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLog.Contrib")]
-[assembly: AssemblyDescription("Community contributed additions to NLog.")]
+[assembly: AssemblyDescription("Community contributed additions to NLog.  Specifically, a new MappedDiagnosticsLogicalContext for maintaining diagnostic state across threads including asynchronous contexts.  This is similar to ThreadLogicalContext in the log4net library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kim Christensen, Todd Meinershagen")]
+[assembly: AssemblyCompany("Kim Christensen, Todd Meinershagen, Bruno Câmara")]
 [assembly: AssemblyProduct("NLog.Contrib")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
